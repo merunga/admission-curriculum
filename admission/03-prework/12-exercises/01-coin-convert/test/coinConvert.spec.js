0d3747089ea53d984ba28b466d5ddda7be0822b6
Paloma Araujo <39506102+paclaraujo@users.noreply.github.com>
2020-06-27 08:58:53 -0300

    fix(03-prework): fix coin convert test

--- admission/03-prework/12-exercises/01-coin-convert/test/coinConvert.spec.js
+++ admission/03-prework/12-exercises/01-coin-convert/test/coinConvert.spec.js
@@ -18,7 +18,7 @@ describe('coinConvert()', () => {
     Assert.deepEqual(coinConvert(dollars), [soles, pesosMx, pesosCl]);
   });
 
-  it('debería retornar [243.75, 1350, 49500], para  $85', () => {
+  it('debería retornar [276.25, 1350, 49500], para  $85', () => {
     const dollars = 85;
     const soles = dollars * 3.25;
     const pesosMx = dollars * 18;
